774210fd578bee3a9bdf745d07dbb45dfac30bd2
Andy Million <amillion3@gmail.com>
2018-09-01 16:36:52 -0500

    WIP, need to verify heads are complete

--- LegoMiniFigures/Head/HeadBald.cs
+++ LegoMiniFigures/Head/HeadBald.cs
@@ -2,7 +2,14 @@
 
 namespace LegoMiniFigures.Head
 {
-    class HeadBald
+    class HeadBald : HeadBase
     {
+        public override bool HasHair { get { return false; } }
+        public int Shinyness { get; set; }
+
+        public override void Eat()
+        {
+            Console.WriteLine("Bald headed man eats food.");
+        }
     }
 }
--- LegoMiniFigures/Head/HeadHair.cs
+++ LegoMiniFigures/Head/HeadHair.cs
@@ -2,7 +2,15 @@
 
 namespace LegoMiniFigures.Head
 {
-    class HeadHair
+    class HeadHair : HeadBase
     {
+        public override bool HasHair { get { return true; } }
+        public string HairColor { get { return "Brown"; } }
+        public string HairTexture { get { return "Straight"; } }
+
+        public override void Eat()
+        {
+            Console.WriteLine("Brown, straight hair people eat stuff.");
+        }
     }
 }
